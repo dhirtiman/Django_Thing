cc269d25b374604ada1feea6f22c14b079d91b1c
dhirtiman <dhirtimankhati@gmail.com>
2024-02-21 11:49:19 +0530

    some code

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('counter',views.counter,name='counter'),
+    path('register',views.register,name='register')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from .models import Feature
+
 
 # Create your views here.
 
+
 def index(request):
-    return render(request,'index.html')
+
+    features = Feature.objects.all()
+    return render(request, 'index.html', {'features': features})
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Already Used')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Already used')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(
+                    username=username, email=email, password=password)
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request, 'Password Not The same')
+            return redirect('register')
+
+    else:
+        return render(request, 'register.html')
+
+
+def counter(request):
+    text = request.POST['text']
+    length = len(text.split())
+    return render(request, 'counter.html', {'length': length})
